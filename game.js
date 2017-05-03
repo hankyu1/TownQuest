f9c7d7eaf28c6814e22dd9a2a6b3607c58713cbe
Hank Yu <hank.yu@gmail.com>
2017-05-02 22:52:31 -0700

    initial project setup

---
+++ game.js
@@ -0,0 +1,7 @@
+/// <reference path="../../node_modules/excalibur/dist/excalibur.d.ts" />
+var game = new ex.Engine({
+    width: 800,
+    height: 600,
+    displayMode: ex.DisplayMode.FullScreen
+});
+game.start();
---
+++ src/ts/main.ts
@@ -0,0 +1,10 @@
+
+/// <reference path="../../node_modules/excalibur/dist/excalibur.d.ts" />
+
+let game = new ex.Engine({
+    width: 800,
+    height: 600,
+    displayMode: ex.DisplayMode.FullScreen
+});
+
+game.start();
